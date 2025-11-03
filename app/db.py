b075bb5801ba93796f8d130bcb4493d47430934e
DubanPrograDVR <dubanvasquez288@gmail.com>
2025-11-03 12:07:43 -0300

    Algunas actualizaciones

--- app/db.py
+++ app/db.py
@@ -1,9 +1,15 @@
 import sqlite3
 
+NOMBRE_BASE_DE_DATOS = "base_datos.db"
 
-with sqlite3.connect ("base_datos.db") as conn: 
+def crear_tabla ():
+    with sqlite3.connect (NOMBRE_BASE_DE_DATOS) as conn: 
 
-    cur = conn.cursor()
-    cur.execute ("""
-CREATE TABLE IF NOT EXISTS  ()
-""")
+        cur = conn.cursor()
+        cur.execute ("""
+    CREATE TABLE IF NOT EXISTS  ()
+    """)
+    print("Base de datos creada exitosamente")
+
+def conectar (): 
+   return sqlite3.connect (NOMBRE_BASE_DE_DATOS)
---
+++ app/static/js/app.js
@@ -0,0 +1,4 @@
+const grafico = document.getElementById ("#grafico")
+const mi_grafico = new Chart (grafico, {
+    
+}) 
