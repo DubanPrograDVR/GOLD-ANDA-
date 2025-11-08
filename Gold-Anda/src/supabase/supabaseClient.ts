5473f54d7d897b1eea95cb0d4e43274f30851e3a
DubanPrograDVR <dubanvasquez288@gmail.com>
2025-11-08 12:11:10 -0300

    Archivo para establecer conexion a base de datos.

---
+++ Gold-Anda/src/supabase/supabaseClient.ts
@@ -0,0 +1,10 @@
+import { createClient } from '@supabase/supabase-js';
+
+const supabaseUrl = 'https://your-project-ref.supabase.co';
+const supabaseKey = 'your-anon-key';
+
+const supabase = createClient(supabaseUrl, supabaseKey);
+
+export default supabase;
+
+
